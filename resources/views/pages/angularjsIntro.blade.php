c1cde15eac659ed69f0c75c99218e223ccdc8e0d
Eduardo <wilded@live.com>
2017-06-20 17:59:02 +0200

    Working with the ng-model + ng-bind + expressions

--- resources/views/pages/angularjsIntro.blade.php
+++ resources/views/pages/angularjsIntro.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1> About </h1>
-    <p>This is the about page.</p>
+    <h1> AngularJS Example </h1>
+    <div ng-app="">
+        <p>Name:</p> <input type="text" ng-model="name"></p>
+        <p>Last Name:</p> <input type="text" ng-model="lname"></p>
+        <p ng-bind="name + ' ' + lname"></p>
+        <p> My First expression: {{5 + 5}}</p>
+    </div>
+    
  @endsection
