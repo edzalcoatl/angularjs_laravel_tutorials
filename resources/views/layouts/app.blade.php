0ce94d3df552ab0a27b3b7174910b44aa1d0bd2e
Eduardo <wilded@live.com>
2017-06-20 17:47:45 +0200

    Added Script for AngularJS for the main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
     <!-- Scripts -->    
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="/laravel/lsapp//vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
     <script>
         CKEDITOR.replace('article-ckeditor');
     </script>
