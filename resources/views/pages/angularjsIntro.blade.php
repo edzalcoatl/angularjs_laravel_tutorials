17f8c5b56bb8cf591583c21d134bfaca6a74726e
Eduardo <wilded@live.com>
2017-06-20 18:23:57 +0200

    Bootstrap Fromat Added

--- resources/views/pages/angularjsIntro.blade.php
+++ resources/views/pages/angularjsIntro.blade.php
@@ -1,12 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1> AngularJS Example </h1>
-    <div ng-app="">
-        <p>Name:</p> <input type="text" ng-model="name"></p>
-        <p>Last Name:</p> <input type="text" ng-model="lname"></p>
-        <p ng-bind="name + ' ' + lname"></p>
-        <p> My First expression: {{5 + 5}}</p>
+<div class="container">
+    <div class="row">
+        <div class = "col-md-7 col-md-offset-2">
+            <h1> AngularJS Example </h1>
+            <div ng-app="">
+                <form>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" ng-model="name" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="lname">Last Name</label>
+                        <input type="text" class="form-control" id="lname" ng-model="lname" placeholder="Last Name">
+                    </div>
+                </form>
+                <div class="alert alert-warning"><span ng-bind="name + ' ' + lname"></span></div>
+                <div class="alert alert-success"><p> My First expression: {{5 + 5}}</p></div>
+            </div>
+        </div>
     </div>
     
+</div>
  @endsection
